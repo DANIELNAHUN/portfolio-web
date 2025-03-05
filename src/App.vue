cf9ccd6cc489d9a2df5fe5a35ed574c9e0387407
DANIELNAHUN <danielnahuncalcinafuentes@gmail.com>
2025-03-04 21:55:28 -0500

    structuring my portfolio

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HomeView from './views/HomeView.vue'
+import AboutView from './views/AboutView.vue'
+import ProjectsView from './views/ProjectsView.vue'
+import ContactView from './views/ContactView.vue'
+</script>
+
+<template>
+  <HomeView/>
+  <AboutView/>
+  <ProjectsView/>
+  <ContactView/>
+</template>
+
+<style>
+</style>
