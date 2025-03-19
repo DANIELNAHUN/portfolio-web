1dc258a11af36bce53fc97bb9411d5130fc5cf3d
Nahun <danielnahuncalcinafuentes@gmail.com>
2025-03-19 16:37:02 -0500

    refactor: mejorar la gestión de proyectos y optimizar el código en ProjectList y ProjectsView

--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -1,4 +1,3 @@
-
 <script setup>
 import { ref, watch, computed, onMounted } from "vue";
 import { useStore } from "vuex";
@@ -10,44 +9,23 @@ import Button from 'primevue/button';
 const store = useStore()
 
 const props = defineProps({
-  paginator: {Boolean, default: false},
-  layout: {String, default: 'grid'}
+    paginator: { Boolean, default: false },
+    layout: { String, default: 'grid' }
 })
-const featuredProjects = computed(()=> store.state.projects)
-const techs = computed(()=> store.state.technologies)
+const featuredProjects = computed(() => store.state.projects)
+const techs = computed(() => store.state.technologies)
 const projects = ref([])
-const sortKey = ref();
-const sortOrder = ref();
-const sortField = ref();
-const sortOptions = ref([
-    {label: 'Price High to Low', value: '!price'},
-    {label: 'Price Low to High', value: 'price'},
-]);
-const onSortChange = (event) => {
-    const value = event.value.value;
-    const sortValue = event.value;
-
-    if (value.indexOf('!') === 0) {
-        sortOrder.value = -1;
-        sortField.value = value.substring(1, value.length);
-        sortKey.value = sortValue;
-    }
-    else {
-        sortOrder.value = 1;
-        sortField.value = value;
-        sortKey.value = sortValue;
-    }
-};
-
-const updateNameTags = () => featuredProjects.value.forEach( project => {
-  let listtechnologies = project.tags;
-  let techsObject = techs.value;
-  let techNames = listtechnologies.map(tech => {
-    let techName = techsObject.find(techObject => techObject.id === tech);
-    return techName.technology;
-  });
-  projects.value.push({ ...project, tags: techNames });
-});
+const updateNameTags = () => {
+    projects.value = [];
+    featuredProjects.value.forEach(project => {
+    let listtechnologies = project.tags;
+    let techsObject = techs.value;
+    let techNames = listtechnologies.map(tech => {
+        let techName = techsObject.find(techObject => techObject.id === tech);
+        return techName.technology;
+    });
+    projects.value.push({ ...project, tags: techNames });
+});}
 
 const layout = ref(props.layout);
 const options = ref(['list', 'grid']);
@@ -66,27 +44,27 @@ const getCategory = (project) => {
     }
 }
 
-watch([featuredProjects, techs],([newFeaturedProjects, newTechs]) =>{
-  if (newFeaturedProjects.length && newTechs.length) {
-    projects.value = [];
-    updateNameTags();
-  }
+watch([featuredProjects, techs], ([newFeaturedProjects, newTechs]) => {
+    if (newFeaturedProjects.length || newTechs.length) {
+        updateNameTags();
+    }
 });
 
 onMounted(() => {
-  if (featuredProjects.value.length<1) {
-    store.dispatch('getProjects');
-  }
-  if (techs.value.length<1) {
-    store.dispatch('getSkills');
-  }
+    if (featuredProjects.value.length < 1) {
+        store.dispatch('getProjects');
+    }
+    if (techs.value.length < 1) {
+        store.dispatch('getSkills');
+    }
+    updateNameTags();
 });
 
 </script>
 
 <template>
     <div class="card">
-        <DataView :value="projects" :layout="layout" :paginator="paginator" :rows="4" >
+        <DataView :value="projects" :layout="layout" :paginator="paginator" :rows="4">
             <template #header>
                 <div class="flex justify-end">
                     <SelectButton v-model="layout" :options="options" :allowEmpty="false">
@@ -94,17 +72,17 @@ onMounted(() => {
                             <i :class="[option === 'list' ? 'pi pi-bars' : 'pi pi-table']" />
                         </template>
                     </SelectButton>
-                    <!-- <Select v-model="sortKey" :options="sortOptions" optionLabel="label" placeholder="Sort By Category" @change="onSortChange($event)"/> -->
                 </div>
             </template>
 
             <template #list="slotProps">
                 <div class="item-list">
                     <div v-for="(item, index) in slotProps.items" :key="item.id">
-                        <div class="icard" :class="{ 'border-t border-surface-200 dark:border-surface-700': index !== 0 }">
+                        <div class="icard"
+                            :class="{ 'border-t border-surface-200 dark:border-surface-700': index !== 0 }">
                             <div class="tag">
                                 <img :src="item.image" :alt="item.title" />
-                                 <div class="category" style="left: 4px; top: 4px">
+                                <div class="category" style="left: 4px; top: 4px">
                                     <Tag :value="item.category" :severity="getCategory(item)"></Tag>
                                 </div>
                             </div>
@@ -117,11 +95,12 @@ onMounted(() => {
                                 <div class="description">
                                     <span>{{ item.description }}</span>
                                     <div class="project-tags">
-                                        <span v-for="(tag, tagIndex) in item.tags" :key="tagIndex">{{tag}}</span>
+                                        <span v-for="(tag, tagIndex) in item.tags" :key="tagIndex">{{ tag }}</span>
                                     </div>
-                                </div>                                
+                                </div>
                                 <div class="action">
-                                    <Button icon="pi pi-eye" label="View Project" :disabled="!item.status_published" style="flex: 0 1 auto; white-space: nowrap;"></Button>
+                                    <Button icon="pi pi-eye" label="View Project" :disabled="!item.status_published"
+                                        style="flex: 0 1 auto; white-space: nowrap;"></Button>
                                 </div>
                             </div>
                         </div>
@@ -135,7 +114,7 @@ onMounted(() => {
                         <div class="icard">
                             <div class="tag">
                                 <div class="cimg">
-                                    <img :src="item.image" :alt="item.title" style="max-width: 300px"/>
+                                    <img :src="item.image" :alt="item.title" style="max-width: 300px" />
                                     <div class="category" style="left: 4px; top: 4px">
                                         <Tag :value="item.category" :severity="getCategory(item)"></Tag>
                                     </div>
@@ -151,12 +130,13 @@ onMounted(() => {
                                     <span>{{ item.description }}</span>
                                 </div>
                                 <div class="project-tags">
-                                    <span v-for="(tag, tagIndex) in item.tags" :key="tagIndex">{{tag}}</span>
+                                    <span v-for="(tag, tagIndex) in item.tags" :key="tagIndex">{{ tag }}</span>
                                 </div>
-                            </div>                            
+                            </div>
                         </div>
                         <div class="action">
-                            <Button icon="pi pi-eye" label="View Project" :disabled="!item.status_published" style="flex: 0 1 auto; white-space: nowrap;"></Button>
+                            <Button icon="pi pi-eye" label="View Project" :disabled="!item.status_published"
+                                style="flex: 0 1 auto; white-space: nowrap;"></Button>
                         </div>
                     </div>
                 </div>
@@ -164,4 +144,3 @@ onMounted(() => {
         </DataView>
     </div>
 </template>
-
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,46 +1,12 @@
 <script setup>
-import { ref, watch, computed, onMounted  } from "vue";
-import { useStore } from 'vuex';
 import ProjectList from "../components/projects/ProjectList.vue";
-
-const store = useStore()
-
-const featuredProjects = computed(()=> store.state.projects)
-const techs = computed(()=> store.state.technologies)
-const projects = ref([])
-
-const updateNameTags = () => featuredProjects.value.forEach( project => {
-  let listtechnologies = project.tags;
-  let techsObject = techs.value;
-  let techNames = listtechnologies.map(tech => {
-    let techName = techsObject.find(techObject => techObject.id === tech);
-    return techName.technology;
-  });
-  projects.value.push({ ...project, tags: techNames });
-});
-
-watch([featuredProjects, techs],([newFeaturedProjects, newTechs]) =>{
-  if (newFeaturedProjects.length && newTechs.length) {
-    projects.value = [];
-    updateNameTags();
-  }
-});
-
-onMounted(() => {
-  if (featuredProjects.value.length<1) {
-    store.dispatch('getProjects');
-  }
-  if (techs.value.length) {
-    store.dispatch('getSkills');
-  }
-});
 </script>
 
 <template>
   <section id="projects" class="projects">
     <div class="container">
       <h2 class="section-title">Featured Projects</h2>
-      <ProjectList paginator="true" layout="list"/>
+      <ProjectList :paginator="Boolean(true)" layout="list"/>
       <div class="view-all">
         <router-link to="/projects" class="btn primary">View All Projects</router-link>
       </div>
