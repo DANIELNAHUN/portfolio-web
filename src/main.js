e6ef8709050b661045af1494bfa4cab431df1008
Nahun <danielnahuncalcinafuentes@gmail.com>
2025-03-20 15:13:38 -0500

    feat: agregar soporte para Google Analytics con vue-gtag

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura';
 import { useReCaptcha, VueReCaptcha } from 'vue-recaptcha-v3';
 import { createHead } from '@vueuse/head';
+import VueGTag from 'vue-gtag'
 
 const app = createApp(App);
 const head = createHead();
@@ -31,5 +32,5 @@ app.use(store);
 app.use(ToastService);
 app.use(head);
 app.use(VueReCaptcha, { siteKey: import.meta.env.VITE_APP_TOKEN_GOOGLE_RECAPTCHA, loaderOptions: { autoHideBadge: false, useReCaptchaNet: true } })
-
+app.use(VueGTag, { config: { id: import.meta.env.VITE_APP_TOKEN_GOOGLE_ANALYTICS } }, router);
 app.mount('#app');
