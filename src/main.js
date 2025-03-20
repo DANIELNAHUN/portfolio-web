71017d321f20e4c102ceedd124ea780c85352e80
Nahun <danielnahuncalcinafuentes@gmail.com>
2025-03-20 11:54:39 -0500

    feat: agregar soporte para la gestión de metadatos con @vueuse/head en HomeView y configuración en main.js

--- src/main.js
+++ src/main.js
@@ -8,8 +8,10 @@ import store from './store'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura';
 import { useReCaptcha, VueReCaptcha } from 'vue-recaptcha-v3';
+import { createHead } from '@vueuse/head';
 
 const app = createApp(App);
+const head = createHead();
 
 app.use(PrimeVue,
     {
@@ -27,7 +29,7 @@ app.use(PrimeVue,
 app.use(router);
 app.use(store);
 app.use(ToastService);
-// app.use(VueReCaptcha , {siteKey: import.meta.env.VITE_APP_TOKEN_GOOGLE_RECAPTCHA});
+app.use(head);
 app.use(VueReCaptcha, { siteKey: import.meta.env.VITE_APP_TOKEN_GOOGLE_RECAPTCHA, loaderOptions: { autoHideBadge: false, useReCaptchaNet: true } })
 
 app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,33 @@ import HeroView from "./HeroView.vue";
 import AboutView from "./AboutView.vue";
 import ProjectsView from "./ProjectsView.vue";
 import ContactView from "./ContactView.vue";
+import { useHead } from '@vueuse/head';
+
+useHead({
+  title: "Home - Daniel Calcina's Web Portfolio",
+  meta: [
+    {
+      name: 'description',
+      content: 'Welcome to the portfolio of Daniel Calcina, web developer. Here you will find information about me, my projects and how to contact me.',
+    },
+    {
+      property: 'og:title',
+      content: "Home - Daniel Calcina's Web Portfolio",
+    },
+    {
+      property: 'og:description',
+      content: 'Welcome to the portfolio of Daniel Calcina, web developer. Here you will find information about me, my projects and how to contact me.',
+    },
+    {
+      property: 'og:url',
+      content: 'https://daniel.calcina.dev/',
+    },
+    // {
+    //   property: 'og:image',
+    //   content: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    // },
+  ],
+});
 </script>
 
 <template>
